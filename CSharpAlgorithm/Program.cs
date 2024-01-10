c933de4be32400b359a250d36acdc4b8e1f414e8
bokob <bmc0520@naver.com>
2024-01-10 22:26:06 +0900

    동적 배열 연습

--- CSharpAlgorithm/Program.cs
+++ CSharpAlgorithm/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Board board = new Board();
+            board.Initialize();
+
             Console.CursorVisible = false;
 
             const int WAIT_TICK = 1000 / 30;
