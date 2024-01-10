0ff1c9282af468f8869a5e1137c54ebcbb574a4b
bokob <bmc0520@naver.com>
2024-01-11 02:52:43 +0900

    스택 큐

---
+++ Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+        }
+    }
+}
