7e381ec97fd43d696230d195e7b732adc5a0a03b
bokob <bmc0520@naver.com>
2024-01-10 23:23:50 +0900

    맵 만들기

--- CSharpAlgorithm/Board.cs
+++ CSharpAlgorithm/Board.cs
@@ -6,80 +6,64 @@ using System.Threading.Tasks;
 
 namespace CSharpAlgorithm
 {
-    class MyLinkedListNode<T>
-    {
-        public T Data;
-        public MyLinkedListNode<T> Next;
-        public MyLinkedListNode<T> Prev;
-    }
-
-    class MyLinkedList<T>
-    {
-        public MyLinkedListNode<T> Head = null; // 첫번째
-        public MyLinkedListNode<T> Tail = null; // 마지막
-        public int Count = 0;
-
-        public MyLinkedListNode<T> AddLast(T data)
-        {
-            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
-            newRoom.Data = data;
-
-            // 만약 아직 방이 아예 없었다면, 새로 추가한 첫번째 방이 Head;
-            if (Head == null)
-                Head = newRoom;
-            
-            // 기존의 마지막 방과 새로 추가되는 방 연결
-            if(Tail != null)
-            {
-                Tail.Next = newRoom;
-                newRoom.Prev = Tail;
-            }
-
-            // 새로 추가되는 방을 마지막 방으로 한다.
-            Tail = newRoom;
-            Count++;
-            return newRoom;
-        }
-
-        public void Remove(MyLinkedListNode<T> room)
-        {
-            if (Head == null)
-                return;
-
-            // 첫 번째 방의 옆 방을 첫번째 방으로 한다.
-            if (Head == room)
-                Head = Head.Next;
-
-            // 기존의 마지막 방의 이전 방을 마지막 방으로 한다.
-            if (Tail == room)
-                Tail = Tail.Prev;
-
-            if (room.Prev != null)
-                room.Prev.Next = room.Next;
-
-            if (room.Next != null)
-                room.Next.Prev = room.Prev;
-
-            Count--;
-        }
-    }
-
-
    class Board
     {
-        public int[] _data = new int[25]; // 배열
-        public MyLinkedList<int> _data3 = new MyLinkedList<int>(); // 연결 리스트
+        const char CIRCLE = '\u25cf';
+        public TileType[,] _tile;
+        public int _size;
 
-        public void Initialize()
+        public enum TileType
         {
-            _data3.AddLast(101);
-            _data3.AddLast(102);
-            MyLinkedListNode<int> node = _data3.AddLast(103);
-            _data3.AddLast(104);
-            _data3.AddLast(105);
+            Empty,
+            Wall
+        }
 
-            _data3.Remove(node);
+        public void Initialize(int size)
+        {
+            _tile = new TileType[size, size];
+            _size = size;
 
+            for(int y = 0; y<_size;y++)
+            {
+                for(int x=0;x<_size;x++)
+                {
+                    if (x == 0 || x == size - 1 || y == 0 || y == size - 1)
+                    {
+                        _tile[y, x] = TileType.Wall;
+                    }
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor precColor = Console.ForegroundColor;
+            for(int y=0;y<_size;y++) 
+            {
+                for(int x= 0;x<_size;x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = precColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch(type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType .Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
         }
     }
 }
