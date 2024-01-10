e6b003e2190e8db9c430423a1eb3f8f2cc8a2d3f
bokob <bmc0520@naver.com>
2024-01-11 00:46:18 +0900

    미로 생성 알고리즘

--- CSharpAlgorithm/Board.cs
+++ CSharpAlgorithm/Board.cs
@@ -20,19 +20,119 @@ namespace CSharpAlgorithm
 
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
-            for(int y = 0; y<_size;y++)
+            GenerateSideWinderTree();
+        }
+
+        void GenerateSideWinderTree()
+        {
+            // 길 막아 버리기
+            for (int y = 0; y < _size; y++)
             {
-                for(int x=0;x<_size;x++)
+                for (int x = 0; x < _size; x++)
                 {
-                    if (x == 0 || x == size - 1 || y == 0 || y == size - 1)
-                    {
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
-                    }
                     else
                         _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길 뚫기
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < _size; x++)
+                {
+                    // 벽으로 막아버린 부분은 패스
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    // 최외곽에서 한쪽 방향으로만 가게 막음
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
+                }
+            }
+        }
+
+        void GenerateByBinaryTree()
+        {
+            // 길 막아 버리기
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길 뚫기
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    // 벽으로 막아버린 부분은 패스
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    // 최외곽에서 한쪽 방향으로만 가게 막음
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    // 1/2 확률로 양방향 중 한쪽만 길 뚫기
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
                 }
             }
         }
