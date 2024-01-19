fac70af807492588b90fd95665f6c7eb2330ce74
bokob <bmc0520@naver.com>
2024-01-19 21:53:10 +0900

    Priority Queue

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -3,68 +3,97 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-    class TreeNode<T>
+    class PriorityQueue
     {
-        public T Data { get; set; }
-        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+        List<int> _heap = new List<int>();
+        public void Push(int data)
+        {
+            // 힙의 맨 끝에 새로운 데이터를 삽입한다.
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+            // 도장깨기 시작
+            while(now > 0)
+            {
+                // 도장개기 시도
+                int next = (now - 1) / 2;
+                if (_heap[now] < _heap[next])
+                    break;
+                
+                // 두 값 교체
+                int temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // 검사 위치 이동
+                now = next;
+            }
+        }
+        public int Pop()
+        {
+            // 반환할 데이터를 따로 저장
+            int ret = _heap[0];
+
+            // 마지막 데이터를 루트로 이동
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            // 역으로 내려가는 도장깨기 시작
+            int now = 0;
+            while(true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now;
+                // 왼쪽 값이 현재값보다 크면, 왼쪽으로 이동
+                if(left <= lastIndex && _heap[next] < _heap[left])
+                    next = left;
+                // 오른쪽 값이 현재값(왼쪽 이동 포함)보다 크면, 오른쪽으로 이동
+                if (right <= lastIndex && _heap[next] < _heap[right])
+                    next = right;
+
+                // 왼쪽 오른쪽 모두 현재값보다 작으면 종료
+                if (next == now)
+                    break;
+
+                // 두 값을 교체
+                int temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // 검사 위치 이동
+                now = next;
+
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+        }
     }
 
     class Program
     {
-        static TreeNode<string> MakeTree()
-        {
-            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
-            {
-                {
-                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
-                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
-                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
-                    node.Children.Add(new TreeNode<string>() { Data = "스토리" });
-                    root.Children.Add(node);
-                }
-                {
-                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
-                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
-                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
-                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
-                    root.Children.Add(node);
-                }
-                {
-                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
-                    node.Children.Add(new TreeNode<string>() { Data = "배경" });
-                    node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
-                    root.Children.Add(node);
-                }
-
-            }
-            return root;
-        }
-
-        static void PrintTree(TreeNode<string> root)
-        {
-            Console.WriteLine(root.Data);
-            foreach(TreeNode<string> child in root.Children)
-                PrintTree(child);
-        }
-
-        static int GetHeight(TreeNode<string> root)
-        {
-            int height = 0;
-
-            foreach(TreeNode<string> child in root.Children)
-            {
-                int newHeight = GetHeight(child) + 1;
-                height = Math.Max(height, newHeight);
-            }
-
-            return height;
-        }
-
         static void Main(string[] args)
         {
-            TreeNode<string> root = MakeTree();
-            PrintTree(root);
-            Console.WriteLine(GetHeight(root));
+            PriorityQueue q = new PriorityQueue();
+            q.Push(20);
+            q.Push(10);
+            q.Push(30);
+            q.Push(90);
+            q.Push(40);
+
+            while(q.Count() > 0)
+            {
+                Console.WriteLine(q.Pop());
+            }
+
         }
     }
 }
